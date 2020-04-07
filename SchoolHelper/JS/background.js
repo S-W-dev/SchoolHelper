132e32b73e44449e162bcdcdf94709097ed662f9
Walter Shewmake <25shewmwalj@washk12.org>
2020-04-07 14:49:01 -0600

    fixed bootstrap errors

--- SchoolHelper/JS/background.js
+++ SchoolHelper/JS/background.js
@@ -7,3 +7,13 @@
 // chrome.storage.sync.get(['key'], function (result) {
 //     console.log('Value currently is ' + result.key);
 // });
+
+  chrome.runtime.onMessage.addListener(function (message, callback) {
+      if (message == 'hello') {
+          sendResponse({
+              greeting: 'welcome!'
+          })
+      } else if (message == 'disconnect') {
+          chrome.runtime.Port.disconnect();
+      }
+  });
