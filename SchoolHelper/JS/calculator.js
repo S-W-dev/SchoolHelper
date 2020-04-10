90009ebcd8f0ad4cf8a75f92c8591988b94dab82
BuildTools <unconfigured@null.spigotmc.org>
2020-04-10 12:55:40 -0600

    Color Picker

--- SchoolHelper/JS/calculator.js
+++ SchoolHelper/JS/calculator.js
@@ -5,6 +5,8 @@ import {
 } from "./classes.js";
 
 var canvas = document.getElementById('graph-canvas'),
+    lineColor = "#a34db3",
+
     c = canvas.getContext('2d'),
     n = 200, // # of line segments
 
@@ -28,13 +30,61 @@ drawCurve();
 initInput();
 startAnimation();
 
+const pickr = Pickr.create({
+    el: '.color-picker',
+    theme: 'nano', // or 'monolith', or 'nano'
+    position: 'top-end',
+
+    swatches: [
+        'rgba(244, 67, 54, 1)',
+        'rgba(233, 30, 99, 0.95)',
+        'rgba(156, 39, 176, 0.9)',
+        'rgba(103, 58, 183, 0.85)',
+        'rgba(63, 81, 181, 0.8)',
+        'rgba(33, 150, 243, 0.75)',
+        'rgba(3, 169, 244, 0.7)',
+        'rgba(0, 188, 212, 0.7)',
+        'rgba(0, 150, 136, 0.75)',
+        'rgba(76, 175, 80, 0.8)',
+        'rgba(139, 195, 74, 0.85)',
+        'rgba(205, 220, 57, 0.9)',
+        'rgba(255, 235, 59, 0.95)',
+        'rgba(255, 193, 7, 1)'
+    ],
+
+    components: {
+
+        // Main components
+        preview: true,
+        opacity: true,
+        hue: true,
+
+        // Input / output Options
+        interaction: {
+            hex: true,
+            rgba: true,
+            hsla: false,
+            hsva: false,
+            cmyk: false,
+            input: true,
+            clear: false,
+            save: true
+        }
+    }
+});
+
+pickr.on('save', (...args) => {
+  var color = args[0].toHEXA().toString();
+  lineColor = color;
+});
+
 function drawCurve() {
   var percentX, percentY, // Between 0 and 1
       mathX, mathY, //Math Coordinates
       xPixel, yPixel; // Canvas Coordinates
 
   c.clearRect(0, 0, canvas.width, canvas.height)
-  c.strokeStyle = 'purple';
+  c.strokeStyle = lineColor;
   c.lineWidth = 1;
   c.beginPath();
   for(var i = 0; i < n; i++) {
