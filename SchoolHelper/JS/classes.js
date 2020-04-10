b1fcced6aea6632b7121e7d955452a65146493a2
Walter Shewmake <25shewmwalj@washk12.org>
2020-04-10 10:55:46 -0600

    made pages appear in navbar

--- SchoolHelper/JS/classes.js
+++ SchoolHelper/JS/classes.js
@@ -104,14 +104,71 @@ export class Data {
 // theme loader
 export function Load() {
     var theme = Options.getItem('theme');
-        $(".navbar").addClass(`navbar-${theme} bg-${theme}`)
-        $("body").addClass(`bg-secondary-${theme}`)
+    $(".navbar").addClass(`navbar-${theme} bg-${theme}`)
+    $("body").addClass(`bg-secondary-${theme}`)
 }
 
 export var Console = {
-    log: function(x) {
+    log: function (x) {
         if (Options.getItem('debug') == 'true') {
             console.log(x);
         }
     }
 };
+
+export function LoadNav() {
+    $.getJSON("../JSON/grade_rules.json", (grade_rules) => {
+
+        Console.log(grade_rules);
+        var grade = Options.getItem('grade');
+        if (grade == 'k') {
+            grade = '0';
+        } else if (grade == 'teacher') grade = '13';
+        grade = parseInt(grade);
+        var current_grade_rules = grade_rules.grade[grade];
+
+        // add in all allowed pages
+        for (var i = 0; i < current_grade_rules.allowed_pages.length; i += 3) {
+            console.log(i);
+            var div = $(`<div class="row"></div>`);
+            try {
+                var col = $(`<div class="col-4"></div>`);
+
+                col.append(`<li class="nav-item"><a href="HTML/${current_grade_rules.allowed_pages[i]}.html" class="nav-link">${current_grade_rules.allowed_pages[i].replace("_", " ")}</a></li>`)
+                div.append(col);
+            } catch {
+                var col = $(`<div class="col-4"></div>`);
+
+                col.append(`<li class="nav-item"><a href="#" class="nav-link"></a></li>`)
+                div.append(col);
+            }
+            try {
+                var col = $(`<div class="col-4"></div>`);
+
+                col.append(`<li class="nav-item"><a href="HTML/${current_grade_rules.allowed_pages[i+1]}.html" class="nav-link">${current_grade_rules.allowed_pages[i+1].replace("_", " ")}</a></li>`)
+                div.append(col);
+            } catch {
+                var col = $(`<div class="col-4"></div>`);
+
+                col.append(`<li class="nav-item"><a href="#" class="nav-link"></a></li>`)
+                div.append(col);
+            }
+
+            try {
+                var col = $(`<div class="col-4"></div>`);
+
+                col.append(`<li class="nav-item"><a href="HTML/${current_grade_rules.allowed_pages[i+2]}.html" class="nav-link">${current_grade_rules.allowed_pages[i+2].replace("_", " ")}</a></li>`)
+                div.append(col);
+                $("#custom_pages").append(``);
+            } catch {
+                var col = $(`<div class="col-4"></div>`);
+
+                col.append(`<li class="nav-item"><a href="#" class="nav-link"></a></li>`)
+                div.append(col);
+            }
+            console.log(col);
+            console.log(div);
+            $("#custom_pages").append(div);
+        }
+    });
+}
