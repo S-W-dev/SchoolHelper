9470892f3413bc8420713a2ba561dcb6554b91db
Walter Shewmake <25shewmwalj@washk12.org>
2020-05-04 21:03:58 -0600

    notifications

--- SchoolHelper/JS/background.js
+++ SchoolHelper/JS/background.js
@@ -1,10 +1,29 @@
 import {
   Options,
   Message,
-  Data
+  Data,
+  Reminders
 } from './classes.js';
 
 chrome.alarms.onAlarm.addListener(function (alarm) {
   console.log("Got an alarm!", alarm);
-  chrome.notifications.create(notificationId, NotificationOptions);
+  var reminder = Reminders.get(alarm.name);
+    chrome.notifications.create({
+      type: "list",
+      title: "[SchoolHelper] Alarm finished.",
+      message: reminder.name + " finished: " + reminder.link,
+      iconUrl: "/SHLogo.png",
+      items: [
+        {title:"Link", 
+        message:reminder.link
+      },
+      {
+        title: "Created",
+        message: reminder.create
+      },
+    {
+      title: "Scheduled Time",
+      message: reminder.date + " " + reminder.time
+    }]
+    }, () => {});
 });
